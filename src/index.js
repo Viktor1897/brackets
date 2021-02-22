d0a207dde5f468b672978a1d34187633aaf9afa0
Viktor Kolosov <vitykolosov@gmail.com>
2021-02-22 20:20:45 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lastStr = '';
+  function bracketsDelete (str) {
+  lastStr = str;
+    for (brackets of bracketsConfig) {
+      str = str.replace(brackets.join(''), '');
+    }
+  return (lastStr==str)? !str : bracketsDelete(str);
+  }
+  return bracketsDelete(str);
 }
